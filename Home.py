082c131909ca25c9b7cec362b7ee7e52289c51ed
Kyung Jae Lee <dlrudwo1269@gmail.com>
2024-06-24 15:51:50 -0400

    Set up local Llama 3 for inference

--- Home.py
+++ Home.py
@@ -1,12 +1,40 @@
 # run command: streamlit run Home.py
 import streamlit as st
 
+import requests
+
+url = "http://localhost:11435/api/chat"
+
+def llama3(prompt):
+    data = {
+        "model": "llama3",
+        "messages": [
+            {
+                "role": "user",
+                "content": prompt
+            }
+        ],
+        "stream": False
+    }
+
+    headers = {
+        "Content-Type": "application/json"
+    }
+
+    response = requests.post(url, headers=headers, json=data)
+    return response.json()["message"]["content"]
+
 st.set_page_config(
     page_title = "QuizLM",
     page_icon = "🤖"
 )
 def process_input(text):
-    return text
+    text = f"""Create ten flashcards from the following text, enclosed in triple quotes:
+    ```
+    {text}
+    ```
+    """
+    return llama3(text)
 
 with st.form(key='source_text'):
     user_input = st.text_area(label="Input text to create flashcards from")
